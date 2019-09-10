9eac9c23fec092215494a0c62fe50100bd1619f3
NickGraus <nick-graus@hotmail.com>
2019-09-10 11:41:50 +0200

    nose tracking finished

--- script/script.js
+++ script/script.js
@@ -1,7 +1,30 @@
+let video;
+let poseNet;
+let noseX = 0;
+let noseY = 0;
+
 function setup() {
-  createCanvas(1000, 500);
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelReady);
+  poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(poses) {
+  if(poses.length > 0) {
+    noseX = poses[0].pose.keypoints[0].position.x;
+    noseY = poses[0].pose.keypoints[0].position.y;
+  }
+}
+
+function modelReady() {
+  console.log('model ready');
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+  image(video, 0, 0);
+
+  fill(255, 0 , 0);
+  ellipse(noseX, noseY, 33);
 }
